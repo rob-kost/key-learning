5667a91b4530c6d2a54ab9a25b8abbb3f6889ce7
Сергей <153189116+rob-kost@users.noreply.github.com>
2026-06-15 01:46:12 +0300

    Update TestDatabase.kt

--- backend/src/test/kotlin/su/itgalley/support/TestDatabase.kt
+++ backend/src/test/kotlin/su/itgalley/support/TestDatabase.kt
@@ -37,7 +37,11 @@ object TestDatabase {
         if (initialized) return
 
         Database.connect(
-            url = "jdbc:h2:mem:keylearning_test;MODE=MySQL;DATABASE_TO_LOWER=TRUE;CASE_INSENSITIVE_IDENTIFIERS=TRUE;DB_CLOSE_DELAY=-1",
+            url = "jdbc:h2:mem:keylearning_test;" +
+                    "MODE=MySQL;" +
+                    "DATABASE_TO_LOWER=TRUE;" +
+                    "CASE_INSENSITIVE_IDENTIFIERS=TRUE;" +
+                    "DB_CLOSE_DELAY=-1",
             driver = "org.h2.Driver",
             user = "sa",
             password = "",
