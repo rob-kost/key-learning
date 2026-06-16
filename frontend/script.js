3257c9e8ae7e882d53d16c5535f4a5d23c505f21
Rob <robkostebl@gmail.com>
2026-06-16 03:42:49 +0300

    Это с мечтой о каких бабках надо деплоить...

--- frontend/script.js
+++ frontend/script.js
@@ -394,7 +394,7 @@ sp.innerHTML = marked.parse(block.description) + btnHTML;
     // ========== ЗАГРУЗКА НАВИГАЦИИ ==========
     async function fetchNavigationData() {
     try {
-        const response = await fetch('http://localhost:8228/api/navigation');
+        const response = await fetch('/api/navigation');
         if (!response.ok) throw new Error(`Ошибка: ${response.status}`);
         return await response.json();
     } catch (error) {
@@ -538,7 +538,7 @@ if (restartLevel) {
     display.style.color = '#aaa';
 
     try {
-        const response = await fetch(`http://localhost:8228/api/levels/${levelId}`);
+        const response = await fetch(`/api/levels/${levelId}`);
         if (!response.ok) throw new Error(`Ошибка: ${response.status}`);
         const levelData = await response.json();
         return levelData;
